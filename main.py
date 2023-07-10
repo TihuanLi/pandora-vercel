c16a8cfa44629ce48fd8a73358f92f3ae0ff31d4
TihuanLi <119105779+TihuanLi@users.noreply.github.com>
2023-07-10 23:13:11 +0800

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,14 @@
-import os
+# -*- coding: utf-8 -*-
 
-from src.pandora_cloud.server import app
+from os import getenv
 
-if __name__=='__main__':
-    app.run(debug=True, port=os.getenv("PORT", default=5000), host='0.0.0.0')
+from src.pandora_cloud.server import ChatBot
+
+_port = getenv('PORT')
+_proxy = getenv('PANDORA_PROXY')
+_debug = getenv('PANDORA_DEBUG', 'false').lower() == 'true'
+_sentry = getenv('PANDORA_SENTRY', 'false').lower() == 'true'
+_listen = getenv('PANDORA_SERVER_LISTEN', 'true').lower() == 'true'
+_server = getenv('PANDORA_SERVER', '0.0.0.0:{}'.format(_port) if _port else '0.0.0.0')
+
+app = ChatBot(proxy=_proxy, debug=_debug, sentry=_sentry, login_local=True).run(_server, listen=_listen)
